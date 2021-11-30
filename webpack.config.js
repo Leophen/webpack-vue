ec135950e00559d886d7a22366276a96ee1ed1d7
Dorkiliu <dorkiliu@tencent.com>
2021-11-30 15:16:57 +0800

    refactor: 调整 HMR 设置

--- webpack.config.js
+++ webpack.config.js
@@ -9,31 +9,31 @@ module.exports = {
   devtool: 'inline-source-map',
   devServer: {
     open: true,
-    hot: true,
-    port: 8080
+    // hot: true,
+    port: 8080,
   },
   resolve: {
-    extensions: ['.js', '.ts', '.vue']
+    extensions: ['.js', '.ts', '.vue'],
   },
   // 入口文件，webpack 会首先从这里开始编译
   entry: {
-    app: './src/main.ts'
+    app: './src/main.ts',
   },
   // 定义了打包后输出的位置，以及对应的文件名
   output: {
     path: path.resolve(__dirname, './dist'),
     // [name] 是个占位符，等价于 entry 中定义的 key 值，即 app
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'webpack 搭建 vue 项目',
       template: path.resolve(__dirname, './public/index.html'),
-      filename: 'index.html'
+      filename: 'index.html',
     }),
     new CleanWebpackPlugin(),
     new friendlyErrorsWebpackPlugin(),
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
   ],
   module: {
     rules: [
@@ -44,9 +44,9 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       // CSS, PostCSS, and Sass
       {
@@ -56,11 +56,11 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              importLoaders: 1
-            }
+              importLoaders: 1,
+            },
           },
-          'postcss-loader'
-        ]
+          'postcss-loader',
+        ],
       },
       {
         test: /\.scss$/,
@@ -69,42 +69,42 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              importLoaders: 1
-            }
+              importLoaders: 1,
+            },
           },
           'sass-loader',
-          'postcss-loader'
-        ]
+          'postcss-loader',
+        ],
       },
       // 加载图片资源
       {
         test: /\.(png|svg|jpg|gif)$/,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       // 加载字体资源
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        type: 'asset/inline'
+        type: 'asset/inline',
       },
       {
         test: /\.ico$/i,
-        use: 'asset/inline'
+        use: 'asset/inline',
       },
       {
         test: /\.text$/i,
-        use: 'asset/source'
+        use: 'asset/source',
       },
       // TypeScript
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       // Vue
       {
         test: /\.vue$/,
-        use: 'vue-loader'
-      }
-    ]
-  }
+        use: 'vue-loader',
+      },
+    ],
+  },
 }
